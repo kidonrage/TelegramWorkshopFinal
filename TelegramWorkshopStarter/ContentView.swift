5e8c88e913ad7abdda9b319c3680c174c6b0e056
Vlad Eliseev <freshymanchan@gmail.com>
2019-11-02 03:32:54 +1000

    Initial Commit

---
+++ TelegramWorkshopStarter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TelegramWorkshopStarter
+//
+//  Created by Vlad Eliseev on 02.11.2019.
+//  Copyright © 2019 Vlad Eliseev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
